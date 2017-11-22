cd008bf83078853d07a8e152ec82915a65a2254a
Robbie Harwood <rharwood@redhat.com>
2017-11-22 17:23:30 -0500

    Add HTTPSPNEGOAuth (and retain HTTPKerberosAuth as a shim)
    Signed-off-by: Robbie Harwood <rharwood@redhat.com>
    Resolves: #1

--- requests_gssapi/__init__.py
+++ requests_gssapi/__init__.py
@@ -7,19 +7,22 @@ adds optional GSSAPI authentication support and supports mutual
 authentication. Basic GET usage:
 
     >>> import requests
-    >>> from requests_gssapi import HTTPKerberosAuth
-    >>> r = requests.get("http://example.org", auth=HTTPKerberosAuth())
+    >>> from requests_gssapi import HTTPSPNEGOAuth
+    >>> r = requests.get("http://example.org", auth=HTTPSPNEGOAuth())
 
 The entire `requests.api` should be supported.
 """
 import logging
 
-from .gssapi_ import HTTPKerberosAuth, REQUIRED, OPTIONAL, DISABLED
+from .gssapi_ import HTTPSPNEGOAuth, REQUIRED, OPTIONAL, DISABLED
 from .exceptions import MutualAuthenticationError
 from .compat import NullHandler
 
 logging.getLogger(__name__).addHandler(NullHandler())
 
-__all__ = ('HTTPKerberosAuth', 'MutualAuthenticationError', 'REQUIRED',
-           'OPTIONAL', 'DISABLED')
+""" Deprecated compatability shim """
+HTTPKerberosAuth = HTTPSPNEGOAuth
+
+__all__ = ('HTTPKerberosAuth', 'HTTPSNPEGOAuth', 'MutualAuthenticationError',
+           'REQUIRED', 'OPTIONAL', 'DISABLED')
 __version__ = '0.11.0'
