c7804faa945ad1b77ba4060531c9f410a5a0ffdd
Robbie Harwood <rharwood@redhat.com>
2017-12-07 15:44:32 -0500

    Make shim into its own wrapper subclass
    Signed-off-by: Robbie Harwood <rharwood@redhat.com>

--- requests_gssapi/compat.py
+++ requests_gssapi/compat.py
@@ -1,7 +1,9 @@
 """
-Compatibility library for older versions of python
+Compatibility library for older versions of python and requests_kerberos
 """
 import sys
+
+from .gssapi_ import REQUIRED, HTTPSPNEGOAuth  # noqa
 
 # python 2.7 introduced a NullHandler which we want to use, but to support
 # older versions, we implement our own if needed.
@@ -13,3 +15,25 @@ else:
     class NullHandler(Handler):
         def emit(self, record):
             pass
+
+
+class HTTPKerberosAuth(HTTPSPNEGOAuth):
+    """Deprecated compat shim; see HTTPSPNEGOAuth instead."""
+    def __init__(self, mutual_authentication=REQUIRED, service="HTTP",
+                 delegate=False, force_preemptive=False, principal=None,
+                 hostname_override=None, sanitize_mutual_error_response=True):
+        HTTPSPNEGOAuth.__init__(
+            self,
+            mutual_authentication=mutual_authentication,
+            service=service,
+            delegate=delegate,
+            force_preemptive=force_preemptive,
+            principal=principal,
+            hostname_override=hostname_override,
+            sanitize_mutual_error_response=sanitize_mutual_error_response)
+
+    def generate_request_header(self, response, host, is_preemptive=False):
+        # This method needs to be shimmed because `host` isn't exposed to
+        # __init__() and we need to derive things from it
+        return HTTPSPNEGOAuth.generate_request_header(self, response, host,
+                                                      is_preemptive)
