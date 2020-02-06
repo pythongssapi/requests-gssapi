87cfda754811b2c2187d6b16128d0ec352fafd96
Robbie Harwood <rharwood@redhat.com>
2020-02-06 13:11:13 -0500

    Fix redos bug around Negotiate expression

--- requests_gssapi/gssapi_.py
+++ requests_gssapi/gssapi_.py
@@ -67,11 +67,10 @@ def _negotiate_value(response):
     else:
         # There's no need to re-compile this EVERY time it is called. Compile
         # it once and you won't have the performance hit of the compilation.
-        regex = re.compile(r'(?:.*,)*\s*Negotiate\s*([^,]*),?', re.I)
+        regex = re.compile(r'Negotiate\s*([^,]*)', re.I)
         _negotiate_value.regex = regex
 
     authreq = response.headers.get('www-authenticate', None)
-
     if authreq:
         match_obj = regex.search(authreq)
         if match_obj:
